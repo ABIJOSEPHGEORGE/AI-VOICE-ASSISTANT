8dd381f50e87685970b89d6500766317ee05e7ea
ABIN JOSEPH GEORGE <abijosephgeorge2gmail.com>
2022-02-13 23:37:13 +0530

    resolved bug with color mode toggling when accordion is pressed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React,{useState,useEffect} from 'react'
+import {ChakraProvider,theme} from '@chakra-ui/react'
+import Navbar from './NavBar/Navbar'
+import Faq from './Faq.jsx'
+import alanBtn from '@alan-ai/alan-sdk-web';
+import {scroller} from 'react-scroll'
 
-function App() {
+const App = () => {
+  const [index, setIndex] = useState(null)
+  const [toggleColorFlag, setToggleColorFlag] = useState(false)
+
+/* ------ALAN AI INTEGRATION------ */
+
+useEffect(() => {
+  alanBtn({
+      key: '512f14a72d462e1144f99e38639a3f412e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: (commandData) => {
+        if (commandData.command === 'gotoFaq') {
+          scroller.scrollTo(`accordion-item-${commandData.faqId}`,{
+            duration:800,
+            delay:0,
+            smooth:'easeInOutQuart'
+          })
+          setIndex(commandData.faqId-1)
+          // Call the client code that will react to the received command
+        }else if (commandData.command === 'toggleColorMode'){
+          setToggleColorFlag(flag => !flag);
+        }
+      },
+  });
+}, []);
+
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Navbar toggleColorFlag={toggleColorFlag}/>
+      <Faq index={index} setIndex={setIndex} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
